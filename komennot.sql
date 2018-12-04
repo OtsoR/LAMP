261d2c058c9b574a6a4739ce94e80a9e5502746f
Otso Rauta <otsorauta@gmail.com>
2018-12-04 22:39:13 +0200

    Updated README.md

--- komennot.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE heppa CHARACTER SET utf8;
-GRANT ALL ON heppa.* TO iron@localhost IDENTIFIED BY 'bear';
-
-USE heppa;
-CREATE TABLE model(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(1024));
-INSERT INTO model(name) VALUES ('marin');
-INSERT INTO model(name) VALUES ('nakamura');
-INSERT INTO model(name) VALUES ('kona');
-
