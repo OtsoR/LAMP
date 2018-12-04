0749d941981a234a5682dcb49040b4f0ce2e19e3
Otso Rauta <otsorauta@gmail.com>
2018-12-04 23:34:33 +0200

    Modded php default-index.php

--- php/default-index.php
+++ php/default-index.php
@@ -1,20 +1,20 @@
 <?php
-//LOGIN
+// MySQL Login
 $user='heppa';
 $password='8iml4Ah0vEjrfQDYOZOV';
-
-//COnnection
-$datab=$user;
-$dsn="mysql:host=localhost;charset=UTF8;dbname=$datab";
-
-//Connectionopen
+// Data Source Name i.e. connection details
+$database=$user;
+$dsn="mysql:host=localhost;charset=UTF8;dbname=$database";
+// Open Connection, create new object of PDO class
 $pdo=new PDO($dsn, $user, $password);
-
-$pdoStat=$pdo->prepare('SELECT * FROM model;');
-$pdoStat->execute();
-$hits=$pdoStat->fetchAll();
-
+// Perform SQL Query
+$pdoStatement=$pdo->prepare('SELECT * FROM model;');
+$pdoStatement->execute();
+$hits=$pdoStatement->fetchAll();
+// Print the $hits Array
 foreach($hits as $row) {
- echo "<p>".$row['id']." ".$row['name']." </p>\n";
+ echo "<p>".$row['id']." ".$row['name']."</p>\n";
 }
 ?>
+
+
